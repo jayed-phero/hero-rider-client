27de8ae24a0cc897a104c2a833bfe95ec0cd0a18
jayed-phero <jayed.programminghero@gmail.com>
2023-03-20 10:32:55 -0700

    user data saving

--- src/api/ImageHosting.js
+++ src/api/ImageHosting.js
@@ -1,3 +1,32 @@
+export const getPersonalPhotoURL = async image => {
+    const formData = new FormData()
+    console.log(image)
+    formData.append('image', image)
+    const url = `https://api.imgbb.com/1/upload?key=737fe6ae676b4e3a65cde4d20252d1d6`
+
+    const res = await fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    const data = await res.json()
+    console.log(data)
+    return data.data.display_url 
+}
+
+export const getImageTwoUrl = async image => {
+    const formData = new FormData()
+    formData.append('image', image)
+    const url = `https://api.imgbb.com/1/upload?key=737fe6ae676b4e3a65cde4d20252d1d6`
+
+    const res = await fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    const data = await res.json()
+    return data.data.display_url 
+}
+
+
 export const getImageUrl = async image => {
     const formData = new FormData()
     formData.append('image', image)
